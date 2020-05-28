e40b0ac4f4f8881cbfd941180285bc83a397ffd7
Kevin Boston <kbos22@gmail.com>
2020-05-28 12:22:41 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,2 +1,12 @@
 class Project
-  
+  attr_accessor :name 
+  attr_reader :backers 
+  @@all = []
+  def initialize(name)
+    @name = name 
+    @backers = []
+  end
+  def sels.all 
+    @@all 
+  end
+end
