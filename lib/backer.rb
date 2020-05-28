1715df8d1ad9ee4369b0f34aa227a1ee530f3822
Kevin Boston <kbos22@gmail.com>
2020-05-28 12:34:17 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,16 +1,12 @@
 class Backer
   attr_accessor :name
   attr_reader :backed_projects
-  @@all = []
   def initialize(name)
     @name = name 
     @backed_projects = []
   end
   def back_project(new_project)
     @backed_projects << new_project
-    new_project.backers.!include(self) ? new_project.add_backer(self) : new_project.backers
-  end
-  def self.all 
-    @@all 
+    new_project.backers << self
   end
 end
