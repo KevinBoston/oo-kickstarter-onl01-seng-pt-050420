d1919d15beb1820aa3308f129baa5ea3f5ef153b
Kevin Boston <kbos22@gmail.com>
2020-05-28 12:34:36 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -6,11 +6,8 @@ class Project
     @title = title 
     @backers = []
   end
-  def self.all 
-    @@all 
-  end
   def add_backer(new_backer)
     @backers << new_backer
-    new_backer.backed_projects !include(self) ? new_backer.back_project(self) : new_backer.backed_projects
+    new_backer.backed_projects << self
   end
 end
